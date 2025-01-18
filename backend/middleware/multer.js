4d42767b803163ec85ad62a3b94f9f4df4f3028f
it <it1.jobtechventure.com>
2025-01-18 15:49:40 +0530

    increase size

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -25,7 +25,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB file size limit
+  limits: { fileSize: 20 * 1024 * 1024 }, // 5 MB file size limit
 });
 
 export{upload}
