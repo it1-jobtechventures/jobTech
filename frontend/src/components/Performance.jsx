c95abe739fca9dcf6b283144007626d473ef8e18
it <it1.jobtechventure.com>
2025-01-23 11:58:31 +0530

    add count up function when page is on that component

--- frontend/src/components/Performance.jsx
+++ frontend/src/components/Performance.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
+import React from 'react';
 import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
 
 const Performance = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
   return (
     <>
-      <main className=' justify-center items-center py-5 flex flex-col'>
-        <h1 className='text-2xl font-bold text-center underline'></h1>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2  align-middle  p-1 mt-5'>
-          <section className='text-2xl text-center bg-yellow-100 hover:bg-yellow-300 hover:text-white font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64'>
+      <main className="justify-center items-center py-5 flex flex-col">
+        <h1 className="text-2xl font-bold text-center underline">Performance Metrics</h1>
+        <div ref={ref} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 align-middle p-1 mt-5">
+          <section className="text-2xl text-center bg-[#3678f4] hover:bg-[#3366cd] font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
             <p>Jobs Posted</p>
-            <CountUp end={34644} duration={2} />
+            {inView && <CountUp end={34644} duration={2} />}
           </section>
-          <section className='text-2xl text-center bg-pink-300 hover:bg-pink-500 hover:text-white font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64'>
+          <section className="text-2xl text-center bg-[#628af6] hover:bg-[#3678f4] font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
             <p>Jobs Posted</p>
-            <CountUp end={34644} duration={2} />
+            {inView && <CountUp end={34644} duration={2} />}
           </section>
-          <section className='text-2xl text-center bg-green-300 hover:bg-green-500 hover:text-white font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64'>
+          <section className="text-2xl text-center bg-[#829cf8] hover:bg-[#628af6] font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
             <p>Jobs Posted</p>
-            <CountUp end={34644} duration={2} />
+            {inView && <CountUp end={34644} duration={2} />}
           </section>
-          <section className='text-2xl text-center bg-orange-300 hover:bg-orange-500 hover:text-white font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64'>
+          <section className="text-2xl text-center bg-[#9eaffa] hover:bg-[#829cf8] font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
             <p>Jobs Posted</p>
-            <CountUp end={34644} duration={2} />
+            {inView && <CountUp end={34644} duration={2} />}
           </section>
         </div>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Performance
-
+export default Performance;
