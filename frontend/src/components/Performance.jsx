7fb6e0471f5244db6bb3b9a70f743bbb945af36e
it <it1.jobtechventure.com>
2025-01-23 18:06:03 +0530

    update style

--- frontend/src/components/Performance.jsx
+++ frontend/src/components/Performance.jsx
@@ -13,19 +13,19 @@ const Performance = () => {
       <main className="justify-center items-center py-5 flex flex-col">
         <h1 className="text-2xl font-bold text-center underline">Performance Metrics</h1>
         <div ref={ref} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 align-middle p-1 mt-5">
-          <section className="text-2xl text-center bg-[#83a4ff] hover:bg-[#3678f4] font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
+          <section className="text-2xl text-center bg-[#83a4ff] hover:bg-[#3678f4] hover:text-white font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
             <p>Jobs Posted</p>
             {inView && <CountUp end={34644} duration={2} />}
           </section>
-          <section className="text-2xl text-center bg-[#83a4ff] hover:bg-[#3678f4] font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
+          <section className="text-2xl text-center bg-[#83a4ff] hover:bg-[#3678f4] hover:text-white font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
             <p>Jobs Posted</p>
             {inView && <CountUp end={34644} duration={2} />}
           </section>
-          <section className="text-2xl text-center bg-[#83a4ff] hover:bg-[#3678f4] font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
+          <section className="text-2xl text-center bg-[#83a4ff] hover:bg-[#3678f4] hover:text-white font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
             <p>Jobs Posted</p>
             {inView && <CountUp end={34644} duration={2} />}
           </section>
-          <section className="text-2xl text-center bg-[#83a4ff] hover:bg-[#3678f4] font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
+          <section className="text-2xl text-center bg-[#83a4ff] hover:bg-[#3678f4] hover:text-white font-bold h-28 rounded-md flex justify-center items-center flex-col space-y-1 md:space-y-3 w-64 md:w-48 lg:w-56 xl:w-64">
             <p>Jobs Posted</p>
             {inView && <CountUp end={34644} duration={2} />}
           </section>
