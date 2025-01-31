b86e4662c9c14b2c06837c4b226f6cb7379b67ba
it <it1.jobtechventure.com>
2025-01-31 17:22:59 +0530

    update colors

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -82,7 +82,7 @@ const Navbar = () => {
         </section>
       </nav>
       {/* Sidebar for Small Screens */}
-      <div className={`fixed top-0 left-0 h-full w-full bg-blue-500 text-white transform ${ isSidebarOpen ? "translate-y-0" : "-translate-y-full"} transition-transform duration-300 ease-in-out z-40`}>
+      <div className={`fixed top-0 left-0 h-full w-full bg-white text-[#ed1c24] text-xl transform ${ isSidebarOpen ? "translate-y-0" : "-translate-y-full"} transition-transform duration-300 ease-in-out z-40`}>
         <div className="flex flex-col items-center justify-center h-full space-y-6">
           <button onClick={toggleSidebar} className="absolute top-6 right-6 text-white text-2xl focus:outline-none">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
